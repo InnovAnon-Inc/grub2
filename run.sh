f336c7fe2b985cfe020a16243643070597365159
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-07 06:21:29 -0500

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 set -exu
+(( ! $# ))
 
-    grep -vq  '^blacklist pcspkr$/'    /etc/modprobe.d/blacklist.conf ||
+     grep -vq '^blacklist pcspkr$/'    /etc/modprobe.d/blacklist.conf ||
 sudo sed -i 's/^blacklist pcspkr$/#&/' /etc/modprobe.d/blacklist.conf
 sudo modprobe pcspkr
+
+cd "$(dirname "$(readlink -f "$0")")"
 ./theme.awk < grub
 
